041c5c614c39ed742f520a21606b769e9c6f5a63
Алексеев Игорь <dimka_Alexs@mail.ru>
2023-04-11 19:18:32 +0300

    first commit

---
+++ Z2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число");
+int A = ReadNumberFromConsole();
+Console.WriteLine("Введите второе число");
+int B = ReadNumberFromConsole();
+int step = A;
+
+int ReadNumberFromConsole()
+{
+    string input = Console.ReadLine();
+    return int.Parse(input);
+}
+
+for (int i = 1; i < B; i++)
+{
+step = step * A;
+}
+Console.WriteLine(step);
---
+++ Z3/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите длинну массива");
+int n = ReadNumberFromConsole();
+int[] array = new int[n];
+
+Console.WriteLine("Введите минимальное значение элемента массива");
+int min = ReadNumberFromConsole();
+Console.WriteLine("Введите максимальное значение элемента массива");
+int max = ReadNumberFromConsole();
+
+
+int ReadNumberFromConsole()
+{
+    string input = Console.ReadLine();
+    return int.Parse(input);
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("{");
+    for (int i = 0; i < array.length - 1; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+    Console.WriteLine(array[^1] + "}");
+}
+void FillArray(int[] array)
+{
+    int[] array = new int.Random().Next(min, max);
+}
+// Я не понимаю как это сделать)
+
+
